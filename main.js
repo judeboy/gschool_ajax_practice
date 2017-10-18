87cd6991f8d35b16895aa1f23f4148bfad2b0021
jmrigali <jmrigali@ymail.com>
2017-10-17 21:53:17 -0600

    added dom manipulation

--- main.js
+++ main.js
@@ -5,8 +5,15 @@ $(document).ready(function() {
   $xhr.done(function(data) {
     //Step 1: Log Data and see what you are getting back as a response
     console.log(data);
-    //Step2: Access your Data
-    
+    //Step 2: Access your Data
+    // let $image = data.current_observation.image.url
+    //Step 3: Manipulate the DOM with your data
+    // let $cardImage= $('.card-img-top');
+    // console.log($cardImage);
+    // for(let i = 0; i < $cardImage.length; i++){
+    //   $cardImage[i].src = $image;
+    // }
+    // console.log($image);
 
   })
 });
